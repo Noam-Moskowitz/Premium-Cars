7235dee7c8f2137999249ca83ca15088b509d459
NoamMoskowitz <noammz101@gmail.com>
2024-08-30 12:01:37 +0300

    img change

--- frontend/src/pages/RentCarPage.tsx
+++ frontend/src/pages/RentCarPage.tsx
@@ -6,8 +6,8 @@ import BookCarForm from "@/components/forms/BookCarForm";
 const RentCarPage = () => {
   return (
     <div className="size-full  flex flex-col item-center justify-between bg-accent">
-      <div className="md:h-[80vh]">
-        <img className="w-[500px] m-auto" src={car.image} alt="Car Image" />
+      <div className="md:h-[80vh] flex items-center">
+        <img className=" md:w-[500px] lg:w-[800px] m-auto" src={car.image} alt="Car Image" />
       </div>
       <CarDetails car={car} />
       <div className="w-full bg-background md:rounded-t-lg shadow-lg flex justify-center items-center py-5 md:absolute md:border-2 bottom-0">
