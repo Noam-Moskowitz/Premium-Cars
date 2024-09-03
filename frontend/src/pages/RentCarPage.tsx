db11581d8d6c4fac9b127d911ead4d2acd6e4f3e
Noam Moskowitz <noammz101@gmail.com>
2024-09-03 12:48:04 +0300

    bug fixes

--- frontend/src/pages/RentCarPage.tsx
+++ frontend/src/pages/RentCarPage.tsx
@@ -6,12 +6,12 @@ import "animate.css";
 
 const RentCarPage = () => {
   return (
-    <div className="size-full  flex flex-col item-center justify-between bg-accent">
+    <div className="size-full h-[90vh] overflow-hidden  flex flex-col item-center justify-between bg-accent">
       <div className="md:h-[70vh] pb-10 flex items-center animate__animated animate__fadeIn">
         <img className=" md:w-[500px] lg:w-[700px] m-auto" src={car.image} alt="Car Image" />
       </div>
       <CarDetails car={car} />
-      <div className="w-full bg-background md:rounded-t-lg animate__animated animate__fadeInUp shadow-lg flex justify-center items-center py-5 md:absolute md:border-2 bottom-0">
+      <div className="w-full bg-background md:rounded-t-lg animate__animated animate__fadeInUp shadow-lg flex justify-center items-center py-5 md:absolute md:border-2 pb-[5vh] bottom-0">
         <BookCarForm carPrice={car.pricePerDay} />
       </div>
     </div>
