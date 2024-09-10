06e4889f26ff25ebcd1b7130235535e3a151a8a0
Noam Moskowitz <noammz101@gmail.com>
2024-09-10 16:55:53 +0300

    made carousel move

--- frontend/src/components/ui/GenericCarousel.tsx
+++ frontend/src/components/ui/GenericCarousel.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import {
   Carousel,
   CarouselContent,
@@ -7,14 +7,17 @@ import {
   CarouselPrevious,
 } from "@/components/ui/carousel";
 import HeroCard from "../cars/HeroCard";
+import Autoplay from "embla-carousel-autoplay";
 
 interface GenericCarouselProps {
   data: any[];
 }
 
 const GenericCarousel: React.FC<GenericCarouselProps> = ({ data }) => {
+  const plugin = useRef(Autoplay({ delay: 5000, stopOnInteraction: true }));
+
   return (
-    <Carousel>
+    <Carousel plugins={[plugin.current]}>
       <CarouselContent>
         {data.map((item, i) => (
           <CarouselItem key={i}>
