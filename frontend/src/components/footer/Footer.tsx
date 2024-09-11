3be4dc44ae30df67b63d60dae7a2d87669412832
Noam Moskowitz <noammz101@gmail.com>
2024-09-11 15:43:39 +0300

    wip footer content

--- frontend/src/components/footer/Footer.tsx
+++ frontend/src/components/footer/Footer.tsx
@@ -1,7 +1,39 @@
 import React from "react";
+import { FaCcMastercard, FaCcVisa } from "react-icons/fa6";
+import { Button } from "../ui/button";
 
 const Footer = () => {
-  return <div className="bg-secondary h-64 ">aaa</div>;
+  return (
+    <footer className="bg-primary  shadow-lg border-t-8 text-background p-3">
+      <p>
+        <span className="font-bold text-lg">©</span> 2024 Premium Cars. All Rights Reserved.
+      </p>
+      <div className="p-3">
+        <h6>We Accept:</h6>
+        <div className="flex gap-5">
+          <FaCcMastercard size={40} />
+          <FaCcVisa size={40} />
+        </div>
+      </div>
+      <div className="flex items-center">
+        <h6>Ready to Get Started?</h6>
+        <Button variant="ghost" className="underline">
+          Sign Up
+        </Button>
+      </div>
+
+      <div>
+        <h6 className="font-bold">Sitemap:</h6>
+        <ul className="underline list-disc list-inside ">
+          <li className="cursor-pointer hover:opacity-55">Home</li>
+          <li className="cursor-pointer hover:opacity-55">My Orders</li>
+          <li className="cursor-pointer hover:opacity-55">About</li>
+          <li className="cursor-pointer hover:opacity-55">Log In</li>
+          <li className="cursor-pointer hover:opacity-55">Register</li>
+        </ul>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
