c19f35534ae1d087638e2564a435cefc89304f91
Noam Moskowitz <noammz101@gmail.com>
2024-09-16 15:03:27 +0300

    finished bookings router

--- backend/routers/bookingsRouter.js
+++ backend/routers/bookingsRouter.js
@@ -1,9 +1,26 @@
 import express from "express";
-import { checkIfAdmin } from "../token/tokenMiddleware";
+import { checkIfAdmin, validateToken } from "../token/tokenMiddleware.js";
 import { BookingController } from "../bookings/BookingController.js";
+import { checkIfUserWhoBookedOrAdmin, validateBooking } from "../bookings/bookingMiddleware.js";
 
 const router = express.Router();
 
 router.get(`/`, checkIfAdmin, BookingController.getAllBookings);
+
+router.get(`/:id`, BookingController.getOneBooking);
+
+router.get(`/user/:userId`, checkIfAdmin, BookingController.getBookingsByUser);
+
+router.get(`/status/:status`, checkIfAdmin, BookingController.getBookingsByStatus);
+
+router.get(`/car/:carId`, BookingController.getBookingsByCar);
+
+router.post(`/`, validateToken, validateBooking, BookingController.addBooking);
+
+router.put(`/:id`, checkIfUserWhoBookedOrAdmin, validateBooking, BookingController.updateBooking);
+
+router.patch(`/:id`, checkIfUserWhoBookedOrAdmin, BookingController.changeBookingStatus);
+
+router.delete(`/:id`, checkIfAdmin, BookingController.deleteBooking);
 
 export default router;
--- backend/routers/mainRouter.js
+++ backend/routers/mainRouter.js
@@ -1,10 +1,12 @@
 import express from "express";
 import usersRouter from "./usersRouter.js";
 import carRouter from "./carsRouter.js";
+import bookingRouter from "./bookingsRouter.js";
 
 const router = express.Router();
 
 router.use(`/users`, usersRouter);
 router.use(`/cars`, carRouter);
+router.use(`/bookings`, bookingRouter);
 
 export default router;
