fd9402730bbb3cba09f18fcdae77415b5e6056cd
Noam Moskowitz <noammz101@gmail.com>
2024-09-29 13:01:35 +0300

    started set up to create data Table

--- frontend/src/store/userSlice.ts
+++ frontend/src/store/userSlice.ts
@@ -1,33 +1,33 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { jwtDecode } from "jwt-decode";
 
-const initialState={
-        _id:null,
-        first:null,
-        last:null,
-        isAdmin:null,
-        iat:null,
-        exp:null
-    }
+const initialState = {
+  _id: null,
+  first: null,
+  last: null,
+  isAdmin: true,
+  iat: null,
+  exp: null,
+};
 
-const userSlice=createSlice({
-    name:`user`,
-    initialState:initialState,
-    reducers:{
-        saveUser:(state,action):any=>{
-            const authToken=action.payload
+const userSlice = createSlice({
+  name: `user`,
+  initialState: initialState,
+  reducers: {
+    saveUser: (state, action): any => {
+      const authToken = action.payload;
 
-            localStorage.setItem(`authToken`,authToken )
-            
-            return jwtDecode(authToken) 
-        },
-        removeUser:(state,action)=>{
-            localStorage.removeItem(`authToken`)
-            
-            return initialState
-        }
-    }
-})
+      localStorage.setItem(`authToken`, authToken);
 
-export const {saveUser,removeUser}=userSlice.actions
-export default userSlice.reducer
+      return jwtDecode(authToken);
+    },
+    removeUser: (state, action) => {
+      localStorage.removeItem(`authToken`);
+
+      return initialState;
+    },
+  },
+});
+
+export const { saveUser, removeUser } = userSlice.actions;
+export default userSlice.reducer;
